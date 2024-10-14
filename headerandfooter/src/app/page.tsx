9c530b80e083dc2945c01dab8c1b71a2769bb13d
Babar Mehmood <babarzaidi1122@gmail.com>
2024-10-14 14:43:38 +0500

    commit

--- headerandfooter/src/app/page.tsx
+++ headerandfooter/src/app/page.tsx
@@ -4,14 +4,33 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1 className='logo'>Welcome To Pak Angro Trader</h1>
-      <ul>
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/about">About</Link></li>
-      <li><Link href="/contact">Contact</Link></li>
-      <li><Link href="/service">Service</Link></li>  
-      </ul>
+      {/* Navbar */}
+      <header className='logo'>
+        <h1>Pak Angro Trader</h1>
+        <ul className='navbar'>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+          <li><Link href="/service">Service</Link></li>  
+        </ul>
+      </header>
+
+      {/* Hero Section */}
+      <section className='hero'>
+        <h1>Fresh Vegetables & Juicy Fruits</h1>
+        <p>
+          Discover a wide variety of farm-fresh vegetables and handpicked, juicy fruits. Whether you're looking for organic greens, ripe tomatoes, or delicious berries, we have it all. Eat healthy, stay fresh!
+        </p>
+      </section>
+
+
+      
+      <footer className='footer'>
+        <p>&copy; 2024 Pak Angro Trader. All Rights Reserved.</p>
+        
+      </footer>
     </div>
+
   )
 }
 
